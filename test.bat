d1109d1f988b7576d3210ce30d899073bd71aaac
RaoYi <raoyi@users.noreply.github.com>
2017-09-08 00:29:50 +0800

    Update test.bat

--- test.bat
+++ test.bat
@@ -32,8 +32,8 @@ echo press any key to continue...
 pause > nul
 
 :LenovoAuditDate
-reg export HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Lenovo\MachineInfo AuditDate.txt
-find /i "AuditDate" AuditDate.txt
+reg export HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Lenovo\MachineInfo MachineInfo.txt
+find /i "AuditDate" MachineInfo.txt
 echo 1.path: HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Lenovo\MachineInfo
 echo 2.check the value of "AuditDate" key
 echo then test case 02042 pass
